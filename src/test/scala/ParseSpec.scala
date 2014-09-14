76edaa759f56f9e172841312c6af8f72f9051580
Hiroshi Sakurai <sakurai777@gmail.com>
2014-09-14 16:58:11 +0900

    added tests

--- src/test/scala/ParseSpec.scala
+++ src/test/scala/ParseSpec.scala
@@ -8,16 +8,73 @@ class PaserSpec extends FlatSpec {
 		assert(EInt(1)==Parser.parse("1"))
 	}
 
-	it should "parse bin operator" in {
-		assert(EBin(EInt(1),EId("+"),EInt(2))==Parser.parse("1+2"))
+	it should "parse infixs" in {
+		assert(EBin(EInt(1),EId("+"),EInt(2))
+			==Parser.parse("1+2"))
+
 		assert(EBin(EBin(EInt(1),EId("+"),EInt(2)),EId("+"),EInt(3))
 			==Parser.parse("1+2+3"))
+
+		assert(EBin(EInt(1),EId("/"),EInt(2))
+			==Parser.parse("1/2"))
+
+		assert(EBin(EInt(1),EId("*"),EInt(2))
+			==Parser.parse("1*2"))
+
+		assert(EBin(EInt(1),EId("-"),EInt(2))
+			==Parser.parse("1-2"))
+
+		assert(EBin(EInt(1),EId("+"),EInt(2))
+			==Parser.parse("1+2"))
+
+		assert(EBin(EInt(1),EId("<="),EInt(2))
+			==Parser.parse("1<=2"))
+
+		assert(EBin(EInt(1),EId(">="),EInt(2))
+			==Parser.parse("1>=2"))
+
+		assert(EBin(EInt(1),EId(">"),EInt(2))
+			==Parser.parse("1>2"))
+
+		assert(EBin(EInt(1),EId("<"),EInt(2))
+			==Parser.parse("1<2"))
+
+		assert(EBin(EInt(1),EId("=="),EInt(2))
+			==Parser.parse("1==2"))
+
+		assert(EBin(EInt(1),EId(".."),EInt(2))
+			==Parser.parse("1..2"))
+
+		assert(EBin(EInt(1),EId("|"),EInt(2))
+			==Parser.parse("1 | 2"))
+
+		assert(EBin(EInt(1),EId("=>"),EInt(2))
+			==Parser.parse("1 => 2"))
+
+		assert(EBin(EInt(1),EId("in"),EInt(2))
+			==Parser.parse("1 in 2"))
 	}
 
-	it should "parse bin operator2" in {
-		assert(EBin(EInt(1),EId("+"),EInt(2))==Parser.parse("1+2"))
-		assert(EBin(EBin(EInt(1),EId("+"),EInt(2)),EId("+"),EInt(3))
-			==Parser.parse("1+2+3"))
+	it should "parse infixrs" in {
+		assert(EBin(EId("a"),EId(","),EInt(2))
+			==Parser.parse("a,2"))
+
+		assert(EBin(EId("a"),EId(":="),EInt(2))
+			==Parser.parse("a:=2"))
+
+		assert(EBin(EId("a"),EId("thn"),EInt(2))
+			==Parser.parse("a thn 2"))
+
+		assert(EBin(EId("a"),EId("els"),EInt(2))
+			==Parser.parse("a els 2"))
+
+		assert(EBin(EId("a"),EId("elf"),EInt(2))
+			==Parser.parse("a elf 2"))
+	}
+
+	it should "parse := operator" in {
+		assert(EBin(EId("a"),EId(":="),EInt(2))
+			==Parser.parse("a:=2"))
 	}
 }
 
